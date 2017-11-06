148d30302d68cf973b55bd500d594c426b5f6556
gentleseal <gentleseal@users.noreply.github.com>
2017-11-06 22:25:24 +0000

    small changes, focus on DO support

--- Provisioner/do.py
+++ Provisioner/do.py
@@ -1,9 +1,11 @@
 from main import *
 import time
+import base64
+import hashlib
 
 class doDroplet(server):
 
-    def __init__(self, accessToken=None, id=None, status=None, name=None, created_at=None, size={}, networks={}, image={}, region={}, rawDropletList=[]):
+    def __init__(self, accessToken=None, id=None, status=None, name=None, created_at=None, size={}, networks={}, image={}, region={}, rawDropletList=[], sshKey = None): #TODO force ssh key to be passed
         self.__accessToken = accessToken
         self.__headers = {'Authorization': 'Bearer ' + self.__accessToken, 'Content-Type': 'application/json'}
         self.__HTTPSClient = HTTPClient(self.__headers, 'api.digitalocean.com')
@@ -17,52 +19,77 @@ class doDroplet(server):
         self.networks = networks
         self.image = image
         self.region = region
+        self.__SSHKey = sshKey
+        self.SSHFingerprint = self.RSAPublicKeyStrToFingerprint(self.__SSHKey)
 
     def fillPropertiesFromDict(self,dict):
         for key, value in dict.items():
             setattr(self, key, value)
 
+    def RSAPublicKeyStrToFingerprint(self, publicKeyStr):
+        key = base64.b64decode(publicKeyStr.strip().split()[1].encode('ascii'))
+        fingerPrint = hashlib.md5(key).hexdigest()
+        return ':'.join(a + b for a, b in zip(fingerPrint[::2], fingerPrint[1::2]))
+
+    def AddKeyIfNonExistant(self):
+        print ('Assigning a Public Key')
+        try:
+            DOresponse = self.__HTTPSClient.get('/v2/account/keys')
+        except Exception as e:
+            print "Could not connect to DO API"
+            print e.message
+            return False, e.message
+        DOresponse.jsonDecode()
+        accountSSHKeys = DOresponse.body["ssh_keys"]
+        if not any(key['fingerprint'] == self.SSHFingerprint for key in accountSSHKeys):
+            print ('The key is not registered with DO, creating it...')
+            body = '''{
+                          "name": "%s",
+                          "public_key": "%s"
+                        }''' % ('A key for {}'.format(self.name),self.__SSHKey)  # todo: sanitize, different ways to set region/size/image
+
+            print 'About to create the following key:'
+            print body
+
+            try:
+                DOresponse = self.__HTTPSClient.post('/v2/account/keys', body)
+            except Exception as e:
+                print "Could not connect to DO API"
+                print e.message
+                return False, e.message
+            DOresponse.jsonDecode()
+            print DOresponse.status
+            if DOresponse.status == 201:
+                print ('Key successfully created!')
+                return True, ''
+            else:
+                print "Failed to create a key! Response from DO API:"
+                print DOresponse
+                return False, DOresponse
+        else:
+            print ('The key already exists!')
+            return True, ''
+
+
+
+
     def provision(self):
 
-        # All of this is currently pointless as defaults are defined in Dockerfile
-        #
-        # # Set default name
-        #
-        # if self.name == "" or self.name is None:
-        #     self.name = 'Example'
-        #
-        # # Set default region
-        #
-        # if 'slug' not in self.region:
-        #     self.region = {'slug':''}
-        #
-        # if self.region['slug'] == '':
-        #     self.region['slug'] = 'lon1'
-        #
-        # # Set default image
-        #
-        # if 'slug' not in self.image:
-        #     self.image = {'slug': ''}
-        #
-        # if self.image['slug'] == '':
-        #     self.image['slug'] = 'ubuntu-14-04-x64'
-        #
-        # # Set default size
-        #
-        # if 'slug' not in self.size:
-        #     self.size = {'slug': ''}
-        #
-        # if self.size['slug'] == '':
-        #     self.size['slug'] = '512mb'
-
         # Define request body
+        status,message = self.AddKeyIfNonExistant()
+        if not status:
+            print ("SSH Key error:")
+            print message
+            exit(1)
+
 
         body = '''{
               "name": "%s",
               "region": "%s",
               "size": "%s",
-              "image": "%s"
-            }''' % (self.name, self.region['slug'], self.size['slug'], self.image['slug']) #todo: sanitize, different ways to set region/size/image
+              "image": "%s",
+              "ssh_keys": ["%s"]
+            }''' % (self.name, self.region['slug'], self.size['slug'], self.image['slug'], self.SSHFingerprint) #todo: sanitize, different ways to set region/size/image
 
         print 'About to create the following droplet:'
         print body
--- Provisioner/start.py
+++ Provisioner/start.py
@@ -25,14 +25,17 @@ if serverProvider == "DO":
     serverSize = "512mb"
     serverImage = "ubuntu-14-04-x64"
     serverLocation = "lon1"
+    sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA3KVkFiPI+5RlTiTKsRkjEZr6ssjYFw9Tk0dzoLKYQH8NOWA13tpSo8r6wT7P+yxXG631wGSBfHyarCpuNO8X2sS7y1zWFVIiDvp1cT4sKGF3kfMPjmt5vrfrp+qEzxHDG9oQqCvnYv1NnhIsb+ZgLG+S56z7ssEx+CPpbUU2RE+27/RYxRNjSZQ7l3eNiQyyvBlPBnK+RK6uccUJhG8KfqWB1hOtlJ7H71Mx0RwiLA6as7OK5PuwqkCN5JhzJs48mRjtRE86R0VwKwny/LuPmTMyyz7JCg38C4PDgEXIJrAfuo/TJDcqiJnxPeX4+neDnmXEeVvUqMUnbVNlk8qZ+w=="
 
-    server = doDroplet(accessToken=accessToken,name=serverName, image={'slug':serverImage},region={'slug':serverLocation},size={'slug':serverSize})
-elif serverProvider == "VT":
-    apiKey = os.environ['API_KEY']
-    DCID = os.environ['DCID']
-    VPSPLANID = os.environ['VPSPLANID']
-    OSID = os.environ['OSID']
-    #server = doDroplet(accessToken=accessToken,name=serverName, image={'slug':serverImage},region={'slug':serverLocation},size={'slug':serverSize})
+    server = doDroplet(accessToken=accessToken,name=serverName, image={'slug':serverImage},region={'slug':serverLocation},size={'slug':serverSize}, sshKey = sshKey)
+
+# TODO VT SUPPORT DISABLED UNTIL FUTURE VERSIONS
+# elif serverProvider == "VT":
+#     apiKey = os.environ['API_KEY']
+#     DCID = os.environ['DCID']
+#     VPSPLANID = os.environ['VPSPLANID']
+#     OSID = os.environ['OSID']
+#     #server = doDroplet(accessToken=accessToken,name=serverName, image={'slug':serverImage},region={'slug':serverLocation},size={'slug':serverSize})
 else:
     print ("Unknown server provider!")
 
@@ -46,6 +49,7 @@ if status is True:
            if k[0] != '_':
                serverPropertyDict[k] = v
         serverPropertyDict['__Infrary__Provider'] = serverProvider
+        serverPropertyDict['__Infrary__SSHKeyFingerprint'] = server.SSHFingerprint
         headers = {'Authorization': 'Bearer ' + OCTOCORE_TOKEN, 'Content-Type': 'application/json'}
         HTTPClient = HTTPClient(headers, OCTOCORE_DOMAIN, OCTOCORE_PORT, False)
         HTTPClient.put(OCTOCORE_SERVER_SUBMIT_PATH, json.dumps(serverPropertyDict))
